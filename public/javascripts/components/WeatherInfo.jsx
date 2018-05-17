e5461c58278f19832a96328f90c96488e9a407de
Rodrigo Barreto <rcabarreto@gmail.com>
2018-05-17 11:12:40 -0300

    some improvements

--- public/javascripts/components/WeatherInfo.jsx
+++ public/javascripts/components/WeatherInfo.jsx
@@ -4,22 +4,24 @@ const WeatherInfo = React.createClass({
   render: function () {
 
     let city = this.props.city;
-    let currently = this.props.currently;
+    // let currently ;
+
+    let {summary, temperature, apparentTemperature, icon} = this.props.currently;
 
     return (
       <main id="weatherInformation" role="main" className="inner cover">
 
         <h1 id="weatherTitle" className="cover-heading">{city}</h1>
-        <h3>{currently.summary}</h3>
+        <h3>{summary}</h3>
 
         <div id="currentWeather" className="metric-stat">
-          <span id="weather-icon" className={"wi wi-forecast-io-"+currently.icon} title={currently.summary}></span>
-          <span id="weather-temp" className="metric-stat-number">{parseInt(currently.temperature)}°</span>
+          <span id="weather-icon" className={"wi wi-forecast-io-"+icon} title={summary}></span>
+          <span id="weather-temp" className="metric-stat-number">{parseInt(temperature)}°</span>
           <span id="weather-unit" className="unit">c</span>
         </div>
 
         <div id="currentWeatherFeelsLike" className="metric-stat">
-          <span id="weather-temp" className="metric-stat-number">Feels like {parseInt(currently.temperature)}°</span>
+          <span id="weather-temp" className="metric-stat-number">Feels like {parseInt(apparentTemperature)}°</span>
           <span id="weather-unit" className="unit">c</span>
         </div>
 
