8f3a7b6e41c7d8a9828140687fc6500d5cb376ea
Rodrigo Barreto <rcabarreto@gmail.com>
2018-06-16 11:26:18 -0300

    little refactoring

--- app/store/configureStore.jsx
+++ app/store/configureStore.jsx
@@ -1,19 +1,19 @@
-import * as redux from 'redux';
+import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
 import {isLoadingReducer, locationReducer, weatherReducer, errorReducer} from 'reducers'
 
 export var configure = (initialState = {}) => {
 
-  let reducer = redux.combineReducers({
+  let reducer = combineReducers({
     isLoading: isLoadingReducer,
     location: locationReducer,
     weather: weatherReducer,
     error: errorReducer
   });
 
-  let store = redux.createStore(reducer, initialState, redux.compose(
-    redux.applyMiddleware(thunk),
+  let store = createStore(reducer, initialState, compose(
+    applyMiddleware(thunk),
     window.devToolsExtension ? window.devToolsExtension() : f => f
   ));
 
