77b66ba9b845e80046e10f0454242e5e770dc208
Rodrigo Barreto <rcabarreto@gmail.com>
2018-11-29 16:35:57 -0200

    added dotenv

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('dotenv').config();
+
 const express = require('express');
 const createError = require('http-errors');
 const helmet = require('helmet')
@@ -7,15 +9,12 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
 const apiRouter   = require('./routes/api')();
 
 const app = express();
 
-
 app.use(helmet());
 app.disable('x-powered-by');
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,12 +25,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
 app.use('/api', apiRouter);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
